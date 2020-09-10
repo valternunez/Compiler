e1e97172d0eb4ff3809ceda3e35e7f75961def8f
Valter <valter.nunez@outlook.com>
2020-09-10 10:54:50 -0500

    qweqw

--- LexerPython/main.py
+++ LexerPython/main.py
@@ -1,1 +1,25 @@
 from lexer import *
+
+
+lex = Lexer("test1.txt")
+
+
+
+test = [1,3,4,".",5,8]
+
+
+
+
+print(lex.scan().toString())
+
+print(lex.scan().toString())
+
+print(lex.scan().toString())
+
+print(lex.scan().toString())
+
+print(lex.scan().toString())
+
+print(lex.scan().toString())
+
+print(lex.scan().toString())
