940c77861e68489df7e5b2170b3734ac3db23f19
Valter <valter.nunez@outlook.com>
2020-09-10 13:45:09 -0500

    me falta checar letras, comments y strings pero ya estan los numero sl menos

--- LexerPython/lexer/AllClasses.py
+++ LexerPython/lexer/AllClasses.py
@@ -1,4 +1,5 @@
 #Valter Nunez - A01206138
+
 #Token Class
 class Token:
     def __init__(self, tag):
@@ -20,19 +21,19 @@ class CharacterString(Token):
         return self.value
 
     def toString(self):
-        return "STRING - VALUE" + str(self.value)
+        return "STRING - VALUE = " + str(self.value)
 
 #Integer Class
 class Integer(Token):
     def __init__(self, value):
-        super().__init___(Tag.INTEGER)
+        super().__init__(Tag.INTEGER)
         self.value = value
     #Getter
     def getValue(self):
         return self.value
 
     def toString(self):
-        return "INTEGER - VALUE" + str(self.value)
+        return "INTEGER - VALUE = " + str(self.value)
 #Real class
 class Real(Token):
     def __init__(self, value):
@@ -43,11 +44,11 @@ class Real(Token):
         return self.value
 
     def toString(self):
-        return "REAL - VALUE" + str(self.value)
+        return "REAL - VALUE = " + str(self.value)
 
 #Tag Class
 class Tag:
-    EOF = 66666
+    EOF = 65535
     PROGRAM = 256
     CONSTANT = 257
     VAR = 258
@@ -94,7 +95,7 @@ class Word(Token):
     def __init__(self, lexeme, tag):
         super().__init__(tag)
         self.lexeme = lexeme
-
+    #Getter
     def getLexeme(self):
         return str(self.lexeme)
 
@@ -153,6 +154,7 @@ class InputFile:
 
 class Lexer:
     def __init__(self, filename):
+        self.file = InputFile(filename)
         self.words = {}
         addToDic(words_list, self.words)
         reserve(Word("program", Tag.PROGRAM), self.words)
@@ -179,15 +181,20 @@ class Lexer:
         reserve(Word("mod", Tag.MOD), self.words)
         reserve(Word("and", Tag.AND), self.words)
         reserve(Word("or", Tag.OR), self.words)
-        self.file = InputFile(filename)
         self.peek = ""
         pass
 
 
-
+    #Basic validation of whether something is reserved or not(in dictionary or not)
     def isReserved (self, key):
         if key in self.words:
-            pass
+            return True
+        return False
+
+    #Reserve function (add stuff to dictionary)
+    def reserve2(self,key):
+        if key in self.words:return self.words[key]
+
 
     def readch(self):
         self.peek = self.file.getChar()
@@ -195,15 +202,17 @@ class Lexer:
     def readch2(self, c):
         self.readch()
         if self.peek != c:
-            self.file.position += 1
+            self.file.position += 1 #Si no vuelves a leer el mismo segundo caracter otra vez
             return False
         self.file.position += 1
         return True
 
+    #Delete blank spaces
     def skipWhiteSpaces(self):
         self.peek = self.file.peekChar()
         while self.peek.isspace():
             self.peek = self.file.getChar()
+
 
     def readCharacterString(self):
         cs = "" + self.peek
@@ -212,9 +221,10 @@ class Lexer:
             cs += self.peek
             self.peek = self.file.getChar()
         cs += self.peek
-        readch()
+        self.readch()
         return CharacterString(cs)
 
+    #Check the comments
     def readComments(self):
         prev = self.file.position
         current = self.file.position + 1
@@ -224,45 +234,85 @@ class Lexer:
         self.file.position = current + 1
         return Token(Tag.COMMENTS)
 
-
+    #Main function that calls everything
     def scan(self):
+        #Cancel all spaces to do the analysis correctly
         self.skipWhiteSpaces()
-
+        #Lots of if's to check if the character is a reserved symbol one or not
         if self.peek == "(":
-            if readch2("*"):
-                readch()
+            if self.readch2("*"):
+                self.readch()
                 return readComments()
             else:
                 return Token("(")
 
         elif self.peek == "<":
             if self.readch2("="):
-                return self.isReserved("<=")
+                return self.reserve2("<=")
             elif self.peek == ">":
-                return self.isReserved("<>")
+                return self.reserve2("<>")
             else:
                 return Token("<")
 
         elif self.peek == ">":
             if self.readch2("="):
-                return self.isReserved(">=")
+                return self.reserve2(">=")
             else:
                 return Token(">")
 
         elif self.peek == "=":
             if self.readch2("="):
-                return self.isReserved("==")
+                return self.reserve2("==")
             else:
                 return Token("=")
 
         elif self.peek == ":":
             if self.readch2("="):
-                return self.isReserved(":=")
+                return self.reserve2(":=")
             else:
                 return Token(":")
 
         elif self.peek == '"':
                 return self.readCharacterString()
+
+
+
+        #Check if characters is a Digit
+        #For Integers
+        if self.peek.isdigit():
+            v = self.peek
+            self.readch()
+            while self.peek.isdigit():
+                v += self.peek
+                self.readch()
+            if self.peek != ".":
+                #cast to Int
+                vAux = int(v)
+                return Integer (vAux)
+            #For Real Numbers
+            x = v
+            x += self.peek
+            while True:
+                self.readch()
+                if not self.peek.isdigit():
+                    break
+                x += self.peek
+            #Cast to float
+            xAux = float(x)
+            return Real(xAux)
+
+        #Check if character is a Letter
+        if self.peek.isalpha():
+            b = self.peek
+            self.readch()
+
+
+        #Ending
+        tok = Token(self.peek)
+        self.readch();
+        return tok;
+
+
 
 def addToDic(dictionary, words):
     for i in dictionary:
--- LexerPython/main.py
+++ LexerPython/main.py
@@ -4,22 +4,14 @@ from lexer import *
 lex = Lexer("test1.txt")
 
 
-
-test = [1,3,4,".",5,8]
-
-
-
-
 print(lex.scan().toString())
-
 print(lex.scan().toString())
-
 print(lex.scan().toString())
-
 print(lex.scan().toString())
-
 print(lex.scan().toString())
-
 print(lex.scan().toString())
-
+print(lex.scan().toString())
+print(lex.scan().toString())
+print(lex.scan().toString())
+print(lex.scan().toString())
 print(lex.scan().toString())
