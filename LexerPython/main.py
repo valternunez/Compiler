884a9cb13a4de585c8969070ce887e5635ff0035
Valter <valter.nunez@outlook.com>
2020-09-10 22:32:52 -0500

    solo falta el run.sh

--- LexerPython/main.py
+++ LexerPython/main.py
@@ -5,31 +5,10 @@ if len(sys.argv) != 2:
     print("usage: main.py file")
 else:
     lex = Lexer(sys.argv[1])
-    with open(sys.argv[1]) as openfileobject:
-        for line in openfileobject:
-            print(lex.scan().toString())
-            print(lex.scan().toString())
-            print(lex.scan().toString())
-            print(lex.scan().toString())
-            print(lex.scan().toString())
-            print(lex.scan().toString())
-            print(lex.scan().toString())
-            print(lex.scan().toString())
-            print(lex.scan().toString())
-            print(lex.scan().toString())
-            print(lex.scan().toString())
-            print(lex.scan().toString())
-            print(lex.scan().toString())
-            print(lex.scan().toString())
-            print(lex.scan().toString())
-            print(lex.scan().toString())
-            print(lex.scan().toString())
-            print(lex.scan().toString())
-            print(lex.scan().toString())
-            print(lex.scan().toString())
-            print(lex.scan().toString())
-            print(lex.scan().toString())
-            print(lex.scan().toString())
-            print(lex.scan().toString())
-            print(lex.scan().toString())
+
+    with open(sys.argv[1]) as f:
+        while True:
+            c = f.read(1)
+            if not c:
+                break
             print(lex.scan().toString())
