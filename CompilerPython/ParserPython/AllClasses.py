968e8c3a1e1618cc71f029eaee5412370d60869a
Valter <valter.nunez@outlook.com>
2020-11-05 13:15:28 -0600

    Parser comentado

---
+++ CompilerPython/ParserPython/AllClasses.py
@@ -0,0 +1,330 @@
+#Valter Nunez - A01206138
+import sys
+#Token Class
+class Token:
+    def __init__(self, tag):
+        self.tag = tag
+    #Getter
+    def getToken(self):
+        return self.tag
+
+    def toString(self):
+        return "TOKEN - VALUE = " + str(self.tag)
+
+#CharacterString class
+class CharacterString(Token):
+    def __init__(self, value):
+        super().__init__(Tag.CHARACTERSTRING)
+        self.value = value
+    #Getter
+    def getValue(self):
+        return self.value
+
+    def toString(self):
+        return "STRING - VALUE = " + str(self.value)
+
+#Integer Class
+class Integer(Token):
+    def __init__(self, value):
+        super().__init__(Tag.INTEGER)
+        self.value = value
+    #Getter
+    def getValue(self):
+        return self.value
+
+    def toString(self):
+        return "INTEGER - VALUE = " + str(self.value)
+#Real class
+class Real(Token):
+    def __init__(self, value):
+        super().__init__(Tag.REAL)
+        self.value = value
+    #Getter
+    def getValue(self):
+        return self.value
+
+    def toString(self):
+        return "REAL - VALUE = " + str(self.value)
+
+#Tag Class
+class Tag:
+    EOF = 65535
+    PROGRAM = 256
+    CONSTANT = 257
+    VAR = 258
+    BEGIN = 259
+    END = 260
+    INTEGER = 261
+    REAL = 262
+    BOOLEAN = 263
+    STRING = 264
+    ASSIGN = 265
+    WRITELN = 266
+    READLN = 267
+    WHILE = 268
+    DO = 269
+    REPEAT = 270
+    UNTIL = 271
+    FOR = 272
+    TO = 273
+    DOWNTO = 274
+    IF = 275
+    THEN = 276
+    ELSE = 277
+    NOT	= 278
+    EQ = 279
+    NEQ = 280
+    GE = 281
+    LE = 282
+    FALSE = 283
+    TRUE = 284
+    DIV = 285
+    MOD = 286
+    AND = 287
+    OR = 288
+    MINUS = 289
+    ID = 290
+    CHARACTERSTRING = 291
+    COMMENTS = 292
+    ERROR = 293;
+
+
+
+#Word Class
+class Word(Token):
+    def __init__(self, lexeme, tag):
+        super().__init__(tag)
+        self.lexeme = lexeme
+    #Getter
+    def getLexeme(self):
+        return str(self.lexeme)
+
+    def toString(self):
+        return "WORD - LEXEME = " + self.lexeme
+
+eq = Word("==", Tag.EQ)
+ne = Word("<>", Tag.NEQ)
+le = Word("<=", Tag.LE)
+ge = Word(">=", Tag.GE)
+minus = Word("minus", Tag.MINUS)
+assign = Word(":=", Tag.ASSIGN)
+true = Word("true", Tag.TRUE)
+false = Word("false", Tag.FALSE)
+
+
+#InputFile Class
+class InputFile:
+    def __init__(self, filename):
+        self.file = open(filename, "r")
+        self.data = []
+        self.position = 0
+        self.size = 0
+        self.columnNumber = 1
+        self.lineNumber = 1
+        aux = self.file.read()
+        self.lines = 0
+        for x in aux:
+            self.size += 1
+            if x == "\n":
+                self.lines += 1
+            self.data.append(x)
+
+
+    def getChar(self):
+        self.position += 1
+        c = self.data[self.position]
+        if c == "\n":
+            self.columnNumber = 1
+            self.lineNumber += 1
+        else:
+            self.columnNumber += 1
+        return c
+
+    def peekChar(self):
+        if self.isEOF():
+            sys.exit()
+        return self.data[self.position]
+
+    def isEOF(self):
+        if self.lineNumber == self.lines + 1:
+            return True
+        else:
+            return False
+
+    def isEOL(self):
+        return isEOF() or peekChar() == "\n"
+
+class Lexer:
+    def __init__(self, filename):
+        self.file = InputFile(filename)
+        self.words = {}
+        reserve(eq, self.words)
+        reserve(ne, self.words)
+        reserve(le, self.words)
+        reserve(ge, self.words)
+        reserve(minus, self.words)
+        reserve(assign, self.words)
+        reserve(true, self.words)
+        reserve(false, self.words)
+        reserve(Word("program", Tag.PROGRAM), self.words)
+        reserve(Word("constant", Tag.CONSTANT), self.words)
+        reserve(Word("var", Tag.VAR), self.words)
+        reserve(Word("begin", Tag.BEGIN), self.words)
+        reserve(Word("end", Tag.END), self.words)
+        reserve(Word("integer", Tag.INTEGER), self.words)
+        reserve(Word("real", Tag.REAL), self.words)
+        reserve(Word("string", Tag.STRING), self.words)
+        reserve(Word("readln", Tag.READLN), self.words)
+        reserve(Word("while", Tag.WHILE), self.words)
+        reserve(Word("do", Tag.DO), self.words)
+        reserve(Word("repeat", Tag.REPEAT), self.words)
+        reserve(Word("until", Tag.UNTIL), self.words)
+        reserve(Word("for", Tag.FOR), self.words)
+        reserve(Word("to", Tag.TO), self.words)
+        reserve(Word("downto", Tag.DOWNTO), self.words)
+        reserve(Word("if", Tag.IF), self.words)
+        reserve(Word("then", Tag.THEN), self.words)
+        reserve(Word("else", Tag.ELSE), self.words)
+        reserve(Word("not", Tag.NOT), self.words)
+        reserve(Word("div", Tag.DIV), self.words)
+        reserve(Word("mod", Tag.MOD), self.words)
+        reserve(Word("and", Tag.AND), self.words)
+        reserve(Word("or", Tag.OR), self.words)
+        self.peek = ""
+        self.active = True
+
+    #Basic validation of whether something is reserved or not(in dictionary or not)
+    def isReserved (self, key):
+        if key in self.words:
+            return True
+        return False
+
+    #Reserve function (add stuff to dictionary)
+    def reserve2(self,key):
+        if key in self.words:return self.words[key]
+
+    def readch(self):
+        self.peek = self.file.getChar()
+
+    def readch2(self, c):
+        self.readch()
+        if self.peek != c:
+            self.file.position += 1 #Si no vuelves a leer el mismo segundo caracter otra vez
+            return False
+        self.file.position += 1 
+        return True
+
+    #Delete blank spaces
+    def skipWhiteSpaces(self):
+        self.peek = self.file.peekChar()
+        while self.peek.isspace():
+            self.peek = self.file.getChar()
+
+    #Detect and read my beloved strings
+    def readCharacterString(self):
+        cs = "" + self.peek
+        self.readch()
+        while self.peek != '"' :
+            cs += self.peek
+            self.readch()
+        cs += self.peek
+        self.readch()
+        return CharacterString(cs)
+
+    #Check the comments
+    def readComments(self):
+        prev = self.peek
+        self.readch()
+        while self.active:
+            if prev == '*' and self.peek == ')':
+                self.readch()
+                break
+            prev = self.peek
+            self.readch()
+        return Token(Tag.COMMENTS);
+
+    #Main function that calls everything
+    def scan(self):
+        #Cancel all spaces to do the analysis correctly
+        self.skipWhiteSpaces()
+        #Lots of if's to check if the character is a reserved symbol one or not
+        if self.peek == '(':
+            self.readch()
+            if self.peek =='*':
+                return self.readComments()
+            return Token('(')
+
+        elif self.peek == "<":
+            if self.readch2("="):
+                return self.reserve2("<=")
+            elif self.peek == ">":
+                return self.reserve2("<>")
+            else:
+                return Token("<")
+
+        elif self.peek == ">":
+            if self.readch2("="):
+                return self.reserve2(">=")
+            else:
+                return Token(">")
+
+        elif self.peek == "=":
+            if self.readch2("="):
+                return self.reserve2("==")
+            else:
+                return Token("=")
+
+        elif self.peek == ":":
+            if self.readch2("="):
+                return self.reserve2(":=")
+            else:
+                return Token(":")
+        elif self.peek == '"':
+                return self.readCharacterString()
+
+        #Check if characters is a Digit
+        #For Integers
+        if self.peek.isdigit():
+            v = self.peek
+            self.readch()
+            while self.peek.isdigit():
+                v += self.peek
+                self.readch()
+            if self.peek != ".":
+                #cast to Int
+                vAux = int(v)
+                return Integer (vAux)
+            #For Real Numbers
+            x = v
+            x += self.peek
+            while True:
+                self.readch()
+                if not self.peek.isdigit():
+                    break
+                x += self.peek
+            #Cast to float
+            xAux = float(x)
+            return Real(xAux)
+
+        #Check if character is a Letter
+        if self.peek.isalpha():
+            b = self.peek
+            self.readch()
+            while self.peek.isalpha() or self.peek.isdigit():
+                b += self.peek
+                self.readch()
+            s = str(b)
+            if self.isReserved(s):
+                return self.reserve2(s)
+            w = Word(s, Tag.ID)
+            reserve(w, self.words)
+            return w
+
+        #Ending
+        tok = Token(self.peek)
+        self.readch();
+        return tok;
+
+
+def reserve(w, words):
+    words[w.lexeme] = w
--- CompilerPython/ParserPython/parser.py
+++ CompilerPython/ParserPython/parser.py
@@ -1,99 +1,108 @@
+#Valter Nunez
+#A01206138
+#Parser Proyecto
+#Proyecto realizado en conjunto con Martin Vivanco
+
+#Imports
 import sys
 import lexer
 
+#Class del Parser como tal
 class Parser:
-    "Class to check gramar of the input and returns accepted or not"
+
+    #Constructor
     def _init_(self, filename):
-        self.lexer = lexer.Lexer(filename)
-        self.pila = []
-        sel.pilaEstados = 0
-        self.inputstring = []
-        self.tabla = {}
-        self.fillAction("table2.csv")
+        self.lexer = lexer.Lexer(filename) #Obtener lexer de su archivo
+        self.stack = []
+        sel.stackstates = 0  #Se guardan los e
+        self.inputstring = [] #Columna de Input
+        self.table = {} #Tabla final
+        self.fillAction("table2.csv") #Uso de table desde archivo de comas
         self.fillInputString()
 
+    #Llenado de la table Action.
+    #state 1 -> Para llenar las lalves
+    #state 2 -> Crear arrelgos donde vas a meter los valores
+    #state 3 -> Meter los valores ahora si en los arreglos creadoes en el 2
     def fillAction(self, table):
-        arr = []
-        file = open(table, 'r')
-        Lines = file.readlines()
-        mystate = 1
-        keys = []
-        vals = []
-        for line in Lines:
-            line = line.strip()
-            line = str(line)
-            if mystate == 1:
-                keys = line.split(",")
-                mystate = 2
-            elif mystate == 2:
-                x = line.split(",")
+        arr = [] #Inicializacion
+        file = open(table, 'r') #abrir archivo para lectura
+        Lines = file.readlines() #Leectura por linea del archivo. Por row pues
+        state = 1 #Inicializacion para los states
+        keys = [] #Arreglo de llaves
+        vals = [] #Arreglo de valores
+        for line in Lines: #Lectura row por row
+            line = line.strip() #Remover espacios al inicio y final del string
+            line = str(line) #Convertir valor a string
+            if state == 1: #Revisar si el state es 1.
+                keys = line.split(",") #Separar los strings por cada coma encontrada (Estamos usando el csv que es por comas)
+                state = 2 #Cambiar ahora a state 2
+            elif state == 2: #Ahora revisa state dos.
+                x = line.split(",") #Separar los string por comas
                 for value in x:
                     vals.append([value])
-                mystate = 3
-            elif mystate == 3:
-                x = line.split(",")
+                state = 3 #Cambiar ahora a state 3
+            elif state == 3: #Ahora revisa state tres.
+                x = line.split(",") #Separar los strings por comas
                 for i in range(len(x)):
                     vals[i].append(x[i])
         for i in range(len(keys)):
-            self.tabla[keys[i]] = vals[i]
+            self.table[keys[i]] = vals[i]
+        file.close() #Cerrar archivo
 
+    #Generar la columna Input String
     def fillInputString(self):
-        self.lexer.start()
-        self.lexer.readch()
+        self.lexer.start() #Correr el lexer
+        self.lexer.readCh() #Read first Char
         token = self.lexer.scan()
         self.inputstring.append(token)
         while self.lexer.reading:
             print("" + str(token))
             token = self.lexer.scan()
             self.inputstring.append(token)
-        self.lexer.input.close()
+        self.lexer.input.close() #Cerrar el Lexer
 
 
-
+    #Funcion principal para hacer el parseo
     def analyze(self):
-        state = self.pilaEstados.top()
-        entrada = self.inputstring[0]
-        action = self.tabla[entrada] [state]
-        if action[0] == 'S':
-            action.replace('S', '')
-            action = int(action)
-            self.pilaEstados.append(action)
-            self.pila.append(entrada)
-            self.inputstring.pop(0)
-        elif action[0] == 'R':
-            action.replace('R', '')
-            action = int(action)
-            file = open('grammar.txt', 'r')
-            lines = file.readLines()
-            file.close()
-            count = 0
-            reduce = ''
-            for line in lines:
-                if count == action:
+        state = self.stackstates.top() #El primer state es el del tope de la stack
+        entrada = self.inputstring[0] #El primer valor dentro del Input String se va a revisar
+        action = self.table[entrada] [state]
+        if action[0] == 'S': #Todos los Shifts empiezan con S, entonces solo necesitamos saber que es S para que sepamos que es un shift.
+            action.replace('S', '') #Borras la S para que solo quede el numero del state a revisar (nada de string pues)
+            action = int(action) #Convertir los valores numericos a numeros como tal (ints)
+            self.stackstates.append(action) #Appendear el numero del state a la stack de states.
+            self.stack.append(entrada) #Appendear el valor del input sting
+            self.inputstring.pop(0) #Sacar el valor del input string
+        elif action[0] == 'R': #Todos los Reduce empiezan con R, entonces solo neceistamos aber que es una R para saber que es un reduce.
+            action.replace('R', '') #Borras la R para que solo quede el numero del state a revisar (nada de string pues)
+            action = int(action) #Convertir los valores numericos a numeros como tal (ints)
+            file = open('grammar.txt', 'r') #Leer la gramatica para saber que state es el que vamos a revisar para la sustitucion.
+            lines = file.readLines() #Lectura por Lineas (rows)
+            file.close() #Cerrar archivo
+            count = 0 #Inizializar contador
+            reduce = '' #Inicializar reduce
+            for line in lines: #Leer cada row
+                if count == action: #Revisar que estas en la linea correcta, dada por el reduce.
                     reduce = line
                     print(line)
-                count = count + 1
-            reduce  = reduce.strip()
-            reduce = reduce.split(' ')
-            print (reduce)
-            while reduce[-1] != '->':
-                self.pilaEstados.pop()
-                if reduce.pop() != self.pila.pop()
-                print("ERROR")
-                sys.exit()
-            reduce.pop()
-            self.pila.append(reduce.pop())
-            self.pilaEstados.append(int(self.tabla)) #no se
+                count = count + 1 #Avanzas, linea por linea, hasta llegar a la linea de la gramtica correcta.
+            reduce  = reduce.strip() #Eliminar los espacios al inicio y final del string.
+            reduce = reduce.split(' ') #Separar por espacios
+            print (reduce) #Imprimir el valor
+            while reduce[-1] != '->': #Buscamos en la gramatica el igual para hacer la sustitucion
+                self.stackstates.pop() #Sacas del stack de los estados el de hasta arriba.
+                if reduce.pop() != self.stack.pop(): #Si no es igual el pop del reduce y el del stack, hay un problema.
+                    print("ERROR") #Mandar error
+                    sys.exit() #Matas el programa
+            reduce.pop() #Sacas el valor de hasta arriba del reduce
+            self.stack.append(reduce.pop()) #de lo que sacas del reduce, lo appenead al stack.
+            self.stackstates.append(int(self.table[self.stack[-1]][self.stackstates[-1]])) #A los estados tambien haces append del valor final del stack y stack states.
 
 
-
-
-
-
-
-if len(sys.argv) != 2:
-    print("usage: parser.py inputfile")
-else:
-    parser = Parser(sys.argv[1])
-    print(parser.tabla)
-    print(parser.inputstring)
+if len(sys.argv) != 2: #Revisar que se obtengan, si o si, 2 argumentos para correr el programa.
+    print("usage: parser.py inputfile") #Usage
+else: #Si si te dan dos argumentos, vas a correr el parser.
+    parser = Parser(sys.argv[1]) #Mandas a parser el segundo argumento
+    print(parser.table) #Imprimes la tabla
+    print(parser.inputstring) #Imprimes el input string.
